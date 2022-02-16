90608ecafeb9767722302fa1e6a8511fe0887972
James Blackburn <blackburnfjames@gmail.com>
2022-02-16 10:22:40 +0000

    Intial API Layout

--- api.py
+++ api.py
@@ -1,10 +1,14 @@
+from matplotlib.pyplot import connect
 import mysql.connector
 from fastapi import FastAPI
+from typing import Optional
 import os
+
 
 user = os.environ["SQL_USER"]
 # passwd = os.environ["SQL_PASSWORD"]
 passwd = ""
+
 
 cnx = mysql.connector.connect(
     user=user,
@@ -22,6 +26,29 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/bible_query/{bible_name}")
+async def read_item(
+    bible_name: str, book: Optional[str] = None, chapter: Optional[int] = None
+):
+    sql = f"SELECT * FROM {bible_name}"
+    where_clauses = []
+    if book:
+        where_clauses.append(f'book = "{book}"')
+    if chapter:
+        where_clauses.append(f"chapter = {chapter}")
+
+    if len(where_clauses) >= 1:
+        sql += f" WHERE "
+        sql += where_clauses[0]
+        for clause in where_clauses[1:]:
+            sql += f" AND {clause}"
+        sql += ";"
+
+    cursor.execute(sql)
+
+    return {"bible_name": bible_name, "sql": sql}
 
 
 cursor.close()
